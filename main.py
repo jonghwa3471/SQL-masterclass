b5093a8188200d139f1f01df0e577e3e91f0c7b6
Jonghwa <whdghk3471@gmail.com>
2026-04-09 15:05:49 +0900

    #21.6 Redis Caching

--- main.py
+++ main.py
@@ -1,23 +1,29 @@
+import redis
 import sqlite3
+import json
+
+r = redis.Redis(host="localhost", port=6379, decode_responses=True)
 
 conn = sqlite3.connect("movies_download.db")
-
 cur = conn.cursor()
 
-res = cur.execute(
-    "SELECT movie_id, title FROM movies ORDER BY movie_id"
-)  # 여기서 선택된 데이터들은 아직 내 파이썬 프로그램에(메모리에) 로드된 것이 아니다.
 
-# all_movies = res.fetchall()  # 여기서 fetchall을 해야 실제로 로드가 된 것이다.
+def make_expensive_query():
+    redis_key = "director:movies"
+    cached_results = r.get(redis_key)
+    if cached_results:
+        print("cache hit")
+        return json.loads(cached_results)
+    else:
+        print("cache miss")
+        res = cur.execute("SELECT COUNT(*), director FROM movies GROUP BY director;")
+        all_rows = res.fetchall()
+        r.set(redis_key, json.dumps(all_rows), ex=20)
+        return all_rows
 
-print(res.fetchmany(20))
 
-print(res.fetchone(), res.fetchone(), res.fetchone())
-
-for movie in res:
-    print(
-        movie
-    )  # 순환하면서 모든 영화들을 가져오지만, fetchall처럼 한 번에 메모리에 로드하지는 않아서, 부담이 적다.
+v = make_expensive_query()
 
 conn.commit()
 conn.close()
+r.close()
