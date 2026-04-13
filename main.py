74ab3f551c7c5e736f2d17af0eafd34f11f91237
Jonghwa <whdghk3471@gmail.com>
2026-04-13 11:45:12 +0900

    #21.7 PyMongo

--- main.py
+++ main.py
@@ -1,29 +1,21 @@
-import redis
-import sqlite3
-import json
+from pymongo import MongoClient
 
-r = redis.Redis(host="localhost", port=6379, decode_responses=True)
+client = MongoClient("mongodb://localhost:27017")
 
-conn = sqlite3.connect("movies_download.db")
-cur = conn.cursor()
+database = client.get_database("movies")
+movies = database.get_collection("movies")
 
+query = {"director": "Christopher Nolan"}
 
-def make_expensive_query():
-    redis_key = "director:movies"
-    cached_results = r.get(redis_key)
-    if cached_results:
-        print("cache hit")
-        return json.loads(cached_results)
-    else:
-        print("cache miss")
-        res = cur.execute("SELECT COUNT(*), director FROM movies GROUP BY director;")
-        all_rows = res.fetchall()
-        r.set(redis_key, json.dumps(all_rows), ex=20)
-        return all_rows
+# results = movies.find(query)
 
+# for movie in results:
+#     print(movie)
 
-v = make_expensive_query()
+new_movie = {"title": "New movie", "director": "Alpachino"}
 
-conn.commit()
-conn.close()
-r.close()
+result = movies.insert_one(new_movie)
+
+print(result)
+
+client.close()
