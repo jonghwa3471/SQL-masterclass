04e99673ed786a18972566737160dba3c417a77b
Jonghwa <whdghk3471@gmail.com>
2026-04-14 09:45:12 +0900

    #21.10 Redis

--- index.ts
+++ index.ts
@@ -1,34 +1,17 @@
-import { Database } from "bun:sqlite";
-import { drizzle } from "drizzle-orm/bun-sqlite";
-import { comments, users } from "./schema";
-import { eq } from "drizzle-orm";
-// import { drizzle } from "drizzle-orm/better-sqlite3";
-// when use npm
+import { createClient } from "redis";
 
-const sqlite = new Database("users.db");
+const client = createClient();
 
-const db = drizzle(sqlite, { logger: true });
+await client.connect();
 
-/* const result = await db
-  .insert(users)
-  .values({
-    username: "nico",
-  })
-  .returning(); */
+await client.flushAll();
 
-/* const result = await db
-  .insert(comments)
-  .values({ payload: "hello drizzle", userId: 1 })
-  .returning(); */
+await client.hSet("users:1", {
+  username: "nico",
+  password: "1234",
+});
 
-// const result = await db.select().from(comments).where(eq(comments.userId, 1));
+const r = await client.hGetAll("users:1");
+console.log(r);
 
-const result = await db
-  .select({
-    user: users.username,
-    comment: comments.payload,
-  })
-  .from(comments)
-  .leftJoin(users, eq(comments.userId, users.userId));
-
-console.log(result);
+await client.close();
